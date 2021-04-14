c76673ee919dce0d701b85fd665f9b236c695550
bunnyiism <rajputpk10@gmail.com>
2021-04-14 20:14:55 +0530

    clock

---
+++ index.js
@@ -0,0 +1,28 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Game from "./Heading";
+import "./index.css";
+import Data from "./Result";
+
+    
+
+
+
+
+          
+                
+ReactDOM.render( 
+    <>
+    <Game imgsrc="12314.jpg"
+        name="Need for speed"
+        rate="4/5"
+    />
+    
+    </>   
+        
+         
+
+,
+    document.getElementById("root")
+    );
